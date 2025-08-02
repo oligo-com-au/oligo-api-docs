e346f9ebd87aae978b36ef3ba666986978332af1
Xiang Zhang <melonq@foxmail.com>
2025-08-02 17:21:58 +0800

    Update site title and description

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,8 +32,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description={`API doc site description of ${siteConfig.title}`}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
